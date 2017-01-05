8d84fced40ce7da13f0b53638593250458c49f9b
luoruiqing <luoruiqing@waqu.com>
2017-01-05 17:56:39 +0800

    json

---
+++ utils/json.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+from json import loads, dumps
+from collections import OrderedDict
+
+
+def loads_order(string):
+    """加载json保证字典顺序"""
+    return loads(string, object_pairs_hook=OrderedDict)
+
+
+def format_json(dict_object, indent=4, *args, **kwargs):
+    """ 格式化为易于查看的字典 """
+    indent = kwargs.pop("indent", indent)
+    return dumps(dict_object, indent=indent, *args, **kwargs)
