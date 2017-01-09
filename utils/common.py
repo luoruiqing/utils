dd7b0e1d76d6d974a4ef3c2818f16160b916924f
luoruiqing <luoruiqing@waqu.com>
2017-01-09 18:09:06 +0800

    百千万换数字

---
+++ utils/common.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+from re import compile
+from recode import recoding
+
+number_regex = compile(r"([\d\.]+)")
+
+
+def convert_number(string):
+    """格式化数字"""
+    string = recoding(string)
+    y, w, q = ("亿", "万", "千")
+    r, n = number_regex.search(string), None
+    if r:
+        n = r.group(1)
+    result = float(n)
+    if y in string and string.index(y) == string.index(n) + len(n):
+        result *= 100000000
+    elif w in string and string.index(w) == string.index(n) + len(n):
+        result *= 10000
+    elif q in string and string.index(q) == string.index(n) + len(n):
+        result *= 1000
+    return int(result)
