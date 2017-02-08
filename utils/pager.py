65565b7971ff410ae467aeebaee58f3b0c259e97
luoruiqing <luoruiqing@waqu.com>
2017-02-08 18:24:00 +0800

    分页器

--- utils/pager.py
+++ utils/pager.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 from math import ceil
+from types import IntType
 
 
 class Pager:
@@ -7,36 +8,47 @@ class Pager:
     分页器: print Pager().page_list >>> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     """
 
-    def __init__(self, page=1, total=None, start=0, limit=20, list_limit=20, strict=False):
-        self.total = limit if total is None else total  # 总条数 认为至少一页
-        _page = (page if page > 0 else 1) - 1  # 当前页数
-        self.max_page = int(ceil(float(self.total) / limit))  # 最大页数
-        self._page = _page if _page < self.max_page else self.max_page - 1
+    def __init__(self, page=1, total=1, start=None, limit=20, list_limit=20):
+        """ :param page: 当前页数
+            :param total: 记录总条数，没有认为至少有一条数据
+            :param start: 开始于多少条
+            :param limit: 每页多少条
+            :param list_limit: 分页器长度
+        """
+        self.limit = limit
         self._start = start
-        self._limit = limit
+        self.total = total if total > 0 else 1
         self.list_limit = list_limit  # 页码合集限制
+        self.max_page = int(ceil(float(self.total) / limit))  # 最大页数
+        if isinstance(start, IntType):  # 如果传入start 优先按照start来
+            start = (start if start >= 0 else 0)
+            self._start = start if start < total else total - limit
+            self._page = (self._start / limit)
+        else:
+            self._page = (page if page > 0 else 1) - 1  # 当前页数
+        self._page = self._page if self._page < self.max_page else self.max_page - 1
 
     @property
     def is_pages(self):
         """ 是否可以分页 """
-        if self.max_page != 1:
+        if self.max_page > 1:
             return True
         return False
 
     @property
     def start(self):
         """开始于多少条"""
-        return self._page * self._limit
+        if isinstance(self._start, IntType):
+            return self._start
+        return self._page * self.limit
 
     @property
     def end(self):
         """本页结束于多少条"""
-        return self.start + self._limit
-
-    @property
-    def limit(self):
-        """每页多少条"""
-        return self._limit
+        r = self.start + self.limit
+        if r > self.total:
+            r = self.total
+        return r
 
     @property
     def is_first(self):
@@ -110,9 +122,9 @@ class Pager:
 
 
 if __name__ == '__main__':
-    # print Pager(page=15, list_limit=10, total=3000).page_list
-    p = Pager(page=10000, list_limit=10, total=300)
-    print "是否可以分页", p.is_pages  # True
+    print "=" * 30, "page=22", "=" * 30
+    p = Pager(page=22, list_limit=5, total=300)
+    print "是否可以分多页", p.is_pages  # True
     print "当前页", p.page  # 15
     print "上一页", p.prev  # 14
     print "下一页", p.next  # 15
@@ -121,9 +133,44 @@ if __name__ == '__main__':
     print "是否是第一页", p.is_first  # False
     print "是否是最后一页", p.is_last  # True
     print "从多少条开始", p.start  # 280
-    print "从多少条结束", p.end  # 300
+    print "到多少条结束", p.end  # 300
     print "共有多少条", p.total  # 300
     print "共有多少页/最大页数", p.max_page  # 15
     print "一页多少条", p.limit  # 20
-    print "需多少个分页", p.list_limit  # 10
-    print "分页后页码列表", p.page_list  # [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    print "需多少个分页", p.list_limit  # 5
+    print "分页后页码列表", p.page_list  # [11, 12, 13, 14, 15]
+    print "=" * 30, "start=-50", "=" * 30
+    p = Pager(start=-50, list_limit=5, total=300)
+    print "是否可以分多页", p.is_pages  # True
+    print "当前页", p.page  # 1
+    print "上一页", p.prev  # 1
+    print "下一页", p.next  # 2
+    print "首页", p.first  # 1
+    print "末页", p.last  # 15
+    print "是否是第一页", p.is_first  # True
+    print "是否是最后一页", p.is_last  # False
+    print "从多少条开始", p.start  # 0
+    print "到多少条结束", p.end  # 20
+    print "共有多少条", p.total  # 300
+    print "共有多少页/最大页数", p.max_page  # 15
+    print "一页多少条", p.limit  # 20
+    print "需多少个分页", p.list_limit  # 5
+    print "分页后页码列表", p.page_list  # [1, 2, 3, 4, 5]
+    print "=" * 30, " total=None", "=" * 30
+    p = Pager(list_limit=5)
+    print "是否可以分多页", p.is_pages  # True
+    print "当前页", p.page  # 1
+    print "上一页", p.prev  # 1
+    print "下一页", p.next  # 2
+    print "首页", p.first  # 1
+    print "末页", p.last  # 15
+    print "是否是第一页", p.is_first  # True
+    print "是否是最后一页", p.is_last  # False
+    print "从多少条开始", p.start  # 0
+    print "到多少条结束", p.end  # 20
+    print "共有多少条", p.total  # 300
+    print "共有多少页/最大页数", p.max_page  # 15
+    print "一页多少条", p.limit  # 20
+    print "需多少个分页", p.list_limit  # 5
+    print "分页后页码列表", p.page_list  # [1, 2, 3, 4, 5]
+    print "=" * 30, " end ", "=" * 30
