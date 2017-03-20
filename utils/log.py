74162e07e98d4a8e4dcf64666f3f8820e4c1e276
luoruiqing <a184187664@qq.com>
2017-03-20 11:51:47 +0800

    日志模块重新整理，添加打印错误，如果有日志模块就将日志输出到日志，否则输出到outerr内

--- utils/log.py
+++ utils/log.py
@@ -3,52 +3,74 @@ from sys import stdout
 from collections import Iterable
 from logging import INFO, DEBUG, NOTSET, basicConfig, getLogger, Logger
 
-
-def log():
-    # 建立日志
-    logger = getLogger()
-    logger.setLevel(DEBUG)
-    logger.info("None")
-    # 屏蔽日志
-    getLogger('requests').setLevel(INFO)  # 屏蔽requests INFO 级别一下的日志
-    # 默认日志
-    basicConfig(  # 全局默认日志
-        level=INFO,
-        format='%(asctime)s [%(levelname)s] %(threadName)s %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-        stream=stdout
-    )
+FORMAT = "[%(levelname)s - %(asctime)s]: %(message)s"
 
 
 def log_wrapper(func):
-    """ 修改打印方法，可以传入其他数据类型，使用str强制转换 """
+    """ 修改打印方法，可以传入其他数据类型，使用str强制转换
+    logger.debug(range(5)) --> [INFO - 2017-03-20 11:50:07]: [0, 1, 2, 3, 4]
+    """
 
-    def wrapper(*args, **kwargs):
+    def wrapper(self, *args, **kwargs):
         args = list(args)
-        if not isinstance(args[2], basestring) and isinstance(args[2], Iterable):
-            args[2] = str(args[2])
-        return func(*args, **kwargs)
+        if not isinstance(args[1], basestring) and isinstance(args[1], Iterable):
+            args[1] = str(args[1]) or repr(args[1])
+        return func(self, *args, **kwargs)
 
     return wrapper
 
 
-Logger._log = log_wrapper(Logger._log)
+Logger._log = log_wrapper(Logger._log)  # 扩展内置方法
+
+
+def logger_init():
+    """ 闭包方法，当遇到错误时，如果有日志模块，将错误输出到日志，如果没有输出到控制台
+
+from logging import basicConfig
+# basicConfig() # 初始化后和未初始化日志模块有区别
+try:
+    1 + "s"
+except:
+    logging_err()
+    """
+    from sys import stderr
+    from traceback import format_exc
+    from logging import root, getLogger, DEBUG
+    logger = getLogger()
+    logger.setLevel(DEBUG)
+
+    def outerr(e=None):
+        e = e or format_exc()
+        if root.handlers:
+            logger.error(e)
+        else:
+            stderr.write(e)
+
+    return outerr
+
+
+logging_err = logger_init()
+del logger_init
 
 
 def get_mongodb_logger(collection, db="log", host='localhost', port=None,
                        username=None, password=None, level=NOTSET):
     # 将日志写入到mongodb中
-    from mongolog.handlers import MongoHandler # mongolog 这是一个log库
+    from mongolog.handlers import MongoHandler  # mongolog 这是一个log库
     logger = getLogger()
     logger.setLevel(DEBUG)
     logger.addHandler(MongoHandler(collection, db))
     return logger
 
 
+del get_mongodb_logger  # 这个方式产生的日志比较详细，占用磁盘空间太高，只记录错误日志还可以
+basicConfig(level=INFO, format=FORMAT, datefmt='%Y-%m-%d %H:%M:%S', stream=stdout)  # 全局默认日志
+getLogger('requests').setLevel(INFO)  # 屏蔽requests INFO 级别一下的日志
+
 if __name__ == '__main__':
-    # logger = get_mongodb_logger('db_test')
-    # logger.info("Hello word!")
     logger = getLogger()
     logger.setLevel(DEBUG)
     logger.info("Hello word!")
     logger.info(range(5))
+    logger.info(256)
+    logger.info("sleep %ss...", 5)
