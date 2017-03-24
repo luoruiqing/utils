6d332f0526523466a2fb0e5adc59b70c11053975
luoruiqing <a184187664@qq.com>
2017-03-24 11:27:59 +0800

    取出方法内常量

--- utils/test_utils/dis_dome.py
+++ utils/test_utils/dis_dome.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 from dis import dis
 
 
@@ -9,5 +10,17 @@ def test():
         return "no"
 
 
+def get_func_constant():
+    """ 取出方法体内定义的常量 """
+
+    def dome():
+        x = 99
+
+    consts = dome.__code__.co_consts  # 所有常量
+    varnames = dome.__code__.co_varnames  # 所有变量名字
+    print consts[varnames.index("x") + 1]
+
+
 if __name__ == '__main__':
     dis(test)
+    get_func_constant()
