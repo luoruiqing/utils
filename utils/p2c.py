c1f8679d71105e4604477f594c527481667539dc
luoruiqing <a184187664@qq.com>
2017-03-31 12:27:15 +0800

    填充

--- utils/p2c.py
+++ utils/p2c.py
@@ -55,7 +55,7 @@ class P2CThread:
         self.threads = []
         Thread(target=self._producer).start()  # 启动一个生产者
         self.producing = True
-        for consumer_number in range(thread_num - 1 or 1):  # 至少双线程
+        for consumer_number in range(thread_num or 1):  # 至少双线程
             t = Thread(target=self._consumer)
             self.threads.append(t)
             t.start()  # 启动多个消费者
