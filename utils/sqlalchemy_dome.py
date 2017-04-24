eae9a0639e29577b38c9ab8ca00397a4cfb74f85
luoruiqing <a184187664@qq.com>
2017-04-24 20:13:11 +0800

    sqlalchemy的通用str方法

--- utils/sqlalchemy_dome.py
+++ utils/sqlalchemy_dome.py
@@ -24,6 +24,35 @@ generate_head = "sqlacodegen --noviews --noconstraints --noindexes "
 get_session = lambda **config: sessionmaker(bind=create_engine("mysql+mysqlconnector:" + template.format(**config)))
 
 
+def setdefault__str__():
+    """
+    sqlalchemy 的所有__str__方法修改成易读的样子 ;)
+    a.py
+        class Tag(Base):
+            __tablename__ = 'tags'
+
+            name = Column(String(255), nullable=False)
+        print Tag(name="luoruiqing") ->  <invest.tools.database.model.Tag object at 0x03BAFD10>
+    b.py
+        from a import Tag
+        setdefault__str__()
+        print Tag(name="luoruiqing") ->   {"name": "luoruiqing"}
+    """
+    try:
+        from model import Base
+    except:
+        return False
+    from copy import copy
+    from json import dumps
+    def default__str__(self):
+        attr = copy(self.__dict__)
+        del attr["_sa_instance_state"]
+        return dumps(attr)
+
+    Base.__str__ = default__str__  # 改变原有的字符输出方法
+    return True
+
+
 def model_generate(**config):
     """ 生成 SQLAlchemy的model对象"""
     outfile = config.pop("outfile", '')
