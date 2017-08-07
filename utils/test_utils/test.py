91e7713e173898c88dcbea44acdcf75a4ce448f8
luoruiqing <lrq184187664@163.com>
2017-08-07 18:28:33 +0800

    添加测试用例

--- utils/test_utils/test.py
+++ utils/test_utils/test.py
@@ -1,16 +1,25 @@
 # coding:utf-8
 from types import StringTypes
 from collections import Iterable
-from unittest import TestLoader, TestSuite, TextTestRunner, main as test_main
+from unittest import TestCase, TestLoader, TestSuite, TextTestRunner, main as test_main
 
 
 def to_items(item, type=tuple):
-    """ 格式化为元祖，迭代类型中不包含字符 1 > (1,)  ["a"] > ["a"] """
+    """ 格式化为元祖，迭代类型中不包含字符 1 > (1,)  ["a"] > ["a"] 
+    >>> print to_items(1)
+    [1]
+    >>> print to_items("abc", type=list)
+    ['abc']
+    >>> print to_items(range(5))
+    [0, 1, 2, 3, 4]
+    >>> from types import GeneratorType
+    >>> case = to_items((x for x in range(5)))
+    >>> isinstance(case, GeneratorType)
+    True
+    """
     if isinstance(item, Iterable) and not isinstance(item, StringTypes):
-        r = item
-    else:
-        r = type([item, ])
-    return r
+        return item
+    return [item, ]
 
 
 def test_cases(cls, cases=(), debug=True):
@@ -19,9 +28,15 @@ def test_cases(cls, cases=(), debug=True):
     :param cases: Test class member models
     :param thread: ...
     :return: None
-    >>> test_cases(cls, "test_dome") #单方法测试
-    >>> test_cases(cls, ["dome1", "test_dome2"]) #多方法测试
-    >>> test_cases(cls) #全部测试
+    # >>> class Test(TestCase):
+    # >>>   def test_dome1(self):
+    # >>>   return self.assertIn(1, range(2))
+    # >>>     def test_dome2(self):
+    # >>>         return self.assertIn(1, range(2))
+    # >>>
+    # >>> test_cases(Test, "test_dome") #单方法测试
+    # >>> test_cases(Test, ["dome1", "test_dome2"]) #多方法测试
+    # >>> test_cases(Test) #全部测试
     '''
 
     cases = to_items(cases)
@@ -39,3 +54,9 @@ def test_cases(cls, cases=(), debug=True):
         TextTestRunner().run(suite)
     else:
         test_main()
+
+
+if __name__ == '__main__':
+    from  doctest import testmod
+
+    testmod()
