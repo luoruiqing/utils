55b2aee5c158725b471efdc8e003478f0fa0fedb
luoruiqing <lrq184187664@163.com>
2019-04-29 12:11:22 +0800

    文件增加时间戳

--- js/vue发包脚本.sh
+++ js/vue发包脚本.sh
@@ -1,19 +1,20 @@
-#!/usr/bin/env bash
-
 # Webpack打包同时推送到服务器
 ADDRESS=$(echo $1 | awk -F ':' '{print $1}')     # 目标服务器
 TARGET_PATH=$(echo $1 | awk -F ':' '{print $2}') # 目标服务器路径
-DATE=$(date +%Y-%m-%d_%H%M%S)                    # 增加打包时间, 不覆盖文件
+DATE=$(date +%Y-%m-%d_%H%M%S)                    # 增加打包时间, 不覆盖包
+PACKET_DIR='./dist '
+
 function build() {
-    cd $(dirname $0)                              # 脚本所在的目录
+    cd $(dirname $0)
     VUE_PROJECT=$(pwd | awk -F '/' '{print $NF}') # 当前目录名称与远端名称相同
-    npm run build                                 # 打包
-    ssh $ADDRESS "mkdir -p $TARGET_PATH"
-    zip -r $VUE_PROJECT\_$DATE.zip ./dist # 压缩本地文件
-    scp $VUE_PROJECT.zip $1               # 推送到目标服务器
-    # rm $VUE_PROJECT.zip # 删除本地包
-    # 解包
-    ssh $ADDRESS "cd $TARGET_PATH && unzip -o $VUE_PROJECT.zip "
+    FILE_NAME=$VUE_PROJECT"_"$DATE".zip"          # 生成文件名
+    # npm run build
+    ssh $ADDRESS "mkdir -p $TARGET_PATH" # 创建目标目录
+    zip -r $FILE_NAME $PACKET_DIR        # 压缩本地文件
+    scp $FILE_NAME $1                    # 推送到目标服务器
+    # # rm $VUE_PROJECT.zip # 删除本地包
+    # # # 解包
+    ssh $ADDRESS "cd $TARGET_PATH && unzip -o $FILE_NAME "
 }
 
 build $1    # 执行
